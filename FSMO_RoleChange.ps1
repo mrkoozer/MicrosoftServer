15574971ec17d37446020af2e6bfd4e65773065f
mrkoozer <mrkoozer@gmail.com>
2019-01-21 11:21:06 -0500

    Update FSMO_RoleChange.ps1

--- FSMO_RoleChange.ps1
+++ FSMO_RoleChange.ps1
@@ -2,10 +2,21 @@
 
 $srvName = “ACD-DEVDC-01”
 
-# NetDOM /query FSMO
+NetDOM /query FSMO
+
+# PDC Emulator – 0
+# RID Master – 1
+# Infrastructure Master – 2
+# Schema Master – 3
+# Domain Naming Master – 4
+
+# Move-ADDirectoryServerOperationMasterRole -Identity $srvName –OperationMasterRole PDCEmulator,RIDMaster,InfrastructureMaster,SchemaMaster,DomainNamingMaster 
 
 Move-ADDirectoryServerOperationMasterRole -Identity $srvName –OperationMasterRole 0
 Move-ADDirectoryServerOperationMasterRole -Identity $srvName –OperationMasterRole 1
 Move-ADDirectoryServerOperationMasterRole -Identity $srvName –OperationMasterRole 2
 Move-ADDirectoryServerOperationMasterRole -Identity $srvName –OperationMasterRole 3
 Move-ADDirectoryServerOperationMasterRole -Identity $srvName –OperationMasterRole 4
+
+NetDOM /query FSMO
+
